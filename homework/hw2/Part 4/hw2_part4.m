7cbaee40fd3f2f88cecdd0c546c1b077edfce366
Parker Brown <pthb35@gmail.com>
2017-12-03 14:33:23 -0800

    part 4

---
+++ homework/hw2/Part 4/hw2_part4.m
@@ -0,0 +1,22 @@
+clear all
+close all
+clc
+
+%%
+load('data.mat')
+
+%%
+
+time = 1/100 * [0:length(theta_a)-1]';
+figure(1)
+plot(time,theta_a)
+hold on
+plot(time,theta_g)
+plot(time,theta_f)
+
+legend('theta\_a', 'theta\_g', 'theta\_f', 'Location', 'NorthEast')
+grid on
+xlabel('time [s]')
+ylabel('theta [rad]')
+
+saveas(figure(1),'data.jpg')
